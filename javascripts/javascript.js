2aecb9dda0da768af6b2205a6c3359e87065336a
Baptiste Lecocq <baptiste.lecocq@gmail.com>
2013-12-27 17:20:41 +0100

    Add main actions

---
+++ javascripts/javascript.js
@@ -0,0 +1,92 @@
+var ipTV = '',
+    TV = {
+  '0': '512',
+  '1': '513',
+  '2': '514',
+  '3': '515',
+  '4': '516',
+  '5': '517',
+  '6': '518',
+  '7': '519',
+  '8': '520',
+  '9': '521',
+  'info': '108',
+  'ok': '352',
+  'pMinus': '403',
+  'pPlus': '402',
+  'return': '158',
+  'volMinus': '114',
+  'volPlus': '115'
+};
+
+$(function () {
+  if (localStorage) {
+    if (localStorage['ipTV'] === undefined) {
+      ipTV = prompt('Entrez l\'ip de votre décodeur TV orange');
+      localStorage['ipTV'] = ipTV;
+    } else {
+      ipTV = localStorage['ipTV'];
+    }
+  } else {
+    ipTV = promt('Entrez l\'ip de votre décodeur TV orange');
+  }
+
+  $(window).keypress(function (e) {
+    console.log(e.which)
+    switch (e.which) {
+      case 13:
+        performAction('ok');
+        break;
+      case 45:
+        performAction('volMinus');
+        break;
+      case 48:
+        performAction(0);
+        break;
+      case 49:
+        performAction(1);
+        break;
+      case 50:
+        performAction(2);
+        break;
+      case 51:
+        performAction(3);
+        break;
+      case 52:
+        performAction(4);
+        break;
+      case 53:
+        performAction(5);
+        break;
+      case 54:
+        performAction(6);
+        break;
+      case 55:
+        performAction(7);
+        break;
+      case 56:
+        performAction(8);
+        break;
+      case 57:
+        performAction(9);
+        break;
+      case 63:
+        performAction('volPlus');
+        break;
+      case 114:
+        performAction('return');
+        break;
+    }
+  });
+});
+
+function createXHR(method, url) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url, true);
+  return xhr;
+}
+
+function performAction(code) {
+  var xhr = createXHR('GET', 'http://' + ipTV + ':8080/remoteControl/cmd?operation=01&key=' + TV[code] + '&mode=0');
+  xhr.send();
+}
